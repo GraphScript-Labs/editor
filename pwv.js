9b60dff3ea89481396f37a3fb1dbffbae762031d
AttAditya <attaditya@users.noreply.github.com>
2025-07-30 04:02:22 +0000

    deploy: e0cde91dada5e121efaa9ecdb94cd45a2eecfa4e

---
+++ pwv.js
@@ -0,0 +1,13 @@
+// window.hasPWV = true;
+// window.pywebview = {
+//   api: {
+//     close: (async () => {}),
+//     toggle_fullscreen: (async () => {}),
+//     save_file: (async () => {}),
+//     load_file: (async () => {}),
+//     load_project_id: (async () => ""),
+//     backup_project: (async () => {}),
+//     restore_project: (async () => ""),
+//     run_project: (async () => {}),
+//   }
+// }
